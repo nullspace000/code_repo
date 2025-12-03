9262a8769af9ad2706b292cb8006caf81c0346cd
nullspace <nullspace000@protonmail.com>
2025-12-03 17:17:50 -0600

    added photo to text tool fr this time

---
+++ photoToText_2.0.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import pytesseract
+import glob
+import os
+
+# Folder containing screenshots
+folder = "/home/null/Pictures/Screenshots"
+
+# Get all PNG files in the folder
+files = glob.glob(os.path.join(folder, "*.png"))
+
+if not files:
+    raise FileNotFoundError("No PNG files found in the screenshots folder.")
+
+# Get the newest file by modification time
+image_path = max(files, key=os.path.getmtime)
+
+print("Using image:", image_path)
+
+img = Image.open(image_path)
+text = pytesseract.image_to_string(img, lang='eng')
+
+print('###########################')
+print('#                         #')
+print('###########################')
+print(text)
